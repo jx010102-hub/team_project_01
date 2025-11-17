8031e9a65686dab2a6e423fea67dcd1e7c88fb94
sion <jx010102@naver.com>
2025-11-17 17:54:38 +0900

    sion
    1233

--- lib/view/chart_1.dart
+++ lib/view/chart_1.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:team_project_01/model/developer_data.dart';
 
 class Chart1 extends StatefulWidget {
   const Chart1({super.key});
 
   @override
-  State<Chart1> createState() => _Chart1State();
+  State<Chart1> createState() => _ScatterState();
 }
 
-class _Chart1State extends State<Chart1> {
+class _ScatterState extends State<Chart1> {
+  //property
+  late List<DeveloperData> data;
+  late TooltipBehavior tooltipBehavior;
+
+  @override
+  void initState() {
+    super.initState();
+    data = [];
+    tooltipBehavior = TooltipBehavior(enable: true);
+    addData();
+  }
+
+  addData() {
+    //
+    data.add(DeveloperData(year: 2017, developers: 19000));
+    data.add(DeveloperData(year: 2018, developers: 40000));
+    data.add(DeveloperData(year: 2019, developers: 35000));
+    data.add(DeveloperData(year: 2020, developers: 37000));
+    data.add(DeveloperData(year: 2021, developers: 45000));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('dfdf')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Scatter charts')),
+      body: Center(
+        child: SizedBox(
+          width: 380,
+          height: 600,
+          child: SfCartesianChart(
+            title: ChartTitle(
+              text:
+                  'Yearly Growth in the Flutter Community\n\n',
+            ),
+            tooltipBehavior: tooltipBehavior,
+            legend: Legend(isVisible: true),
+            series: [
+              //ColumnSeries : 세로축 막대그래프
+              //BarSeries : 가로축 막대 그래프
+              ScatterSeries<DeveloperData, int>(
+                name: 'site수',
+                dataSource: data,
+                xValueMapper:
+                    (DeveloperData developers, _) =>
+                        developers.year,
+                yValueMapper:
+                    (DeveloperData developers, _) =>
+                        developers.developers,
+                dataLabelSettings: DataLabelSettings(
+                  isVisible: true,
+                ),
+                enableTooltip: true,
+              ),
+            ],
+            // X축을 category로 표현
+            primaryXAxis: CategoryAxis(
+              title: AxisTitle(text: '년도'),
+            ),
+            // y축은 숫자로 표현
+            primaryYAxis: NumericAxis(
+              title: AxisTitle(text: '사이트 수'),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

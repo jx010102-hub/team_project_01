7a55a982e2a40fd477ec9c85467d909617b70c0c
sion <jx010102@naver.com>
2025-11-17 15:38:19 +0900

    first commit

---
+++ lib/view/chart_2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Chart2 extends StatefulWidget {
+  const Chart2({super.key});
+
+  @override
+  State<Chart2> createState() => _Chart2State();
+}
+
+class _Chart2State extends State<Chart2> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

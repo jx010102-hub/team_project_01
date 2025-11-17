f9c838d04bddb6de872506a1b21f7dd4dab1c5f0
sion <jx010102@naver.com>
2025-11-17 15:26:02 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/team_project_01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.team_project_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/view/chart_3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Chart3 extends StatefulWidget {
+  const Chart3({super.key});
+
+  @override
+  State<Chart3> createState() => _Chart3State();
+}
+
+class _Chart3State extends State<Chart3> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

cb879f0cb532405ea9d7f0cb1d6750ae46c8990d
sanghyun97 <sanjungl97@naver.com>
2025-11-17 17:42:56 +0900

    123
     v5f

--- lib/view/chart_3.dart
+++ lib/view/chart_3.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:team_project_01/model/developer_data.dart';
 
 class Chart3 extends StatefulWidget {
   const Chart3({super.key});
@@ -8,11 +10,55 @@ class Chart3 extends StatefulWidget {
 }
 
 class _Chart3State extends State<Chart3> {
+  //Property
+  late List<DeveloperData> data;
+  late TooltipBehavior tooltipBehavior;
+
+  @override
+  void initState() {
+    super.initState();
+    data = [];
+    tooltipBehavior = TooltipBehavior(enable : true);
+    addData();
+  }
+
+  addData(){
+    data.add(DeveloperData(year: 2017, developers: 19000));
+    data.add(DeveloperData(year: 2018, developers: 40000));
+    data.add(DeveloperData(year: 2019, developers: 35000));
+    data.add(DeveloperData(year: 2020, developers: 37000));
+    data.add(DeveloperData(year: 2021, developers: 45000));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('상현'),
+        title: Text('Bar Chart'),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          height: 550,
+          child: SfCircularChart(
+            title: ChartTitle(
+              text: 'Yearly Growth in the Flutter Coummunity'
+            ),
+            tooltipBehavior: tooltipBehavior, // 눌렀을때 수치 뜨게 하는 법
+            series: <CircularSeries<DeveloperData, int>>[
+              PieSeries<DeveloperData, int>(
+                name: 'Site',
+                dataSource: data,
+                explode: true, // 한조각만 띄어내는거
+                selectionBehavior: SelectionBehavior(enable: true), // 한조각만 눌러보는 거
+                xValueMapper: (DeveloperData developers, _) => developers.year,
+                yValueMapper: (DeveloperData developers, _) => developers.developers,
+                dataLabelSettings: DataLabelSettings(isVisible: true),
+                enableTooltip: true,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
